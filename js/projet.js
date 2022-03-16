5e3a7206551aa016e7522170fe2d84c01a34945b
jean-francois labbe <jean-francois@hey.com>
2022-03-16 14:46:27 +0100

    Load vox model

--- js/projet.js
+++ js/projet.js
@@ -1,30 +1,45 @@
 import * as THREE from 'three';
+import { VOXLoader, VOXMesh } from './jsm/loaders/VOXLoader.js';
 
-const VoxelLoader = require("three-voxel-loader");
-
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 1000);
+// camera.position.z = 5;
+camera.position.set( 0.175, 0.075, 1 );
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.background = new THREE.Color(0xf400f3)
+scene.add(camera)
+
+const hemiLight = new THREE.HemisphereLight( 0x888888, 0x444444, 1 );
+scene.add( hemiLight );
+const dirLight = new THREE.DirectionalLight( 0xffffff, 0.75 );
+dirLight.position.set( 1.5, 3, 2.5 );
+scene.add( dirLight );
+
+const loader = new VOXLoader();
+loader.load("../models/monu2.vox",
+	function (chunks) {
+		for (let i = 0; i < chunks.length; i++) {
+			const chunk = chunks[i];
+			const mesh = new VOXMesh(chunk);
+			mesh.scale.setScalar(0.0015);
+			scene.add(mesh);
+		}
+	}
+	,undefined, function(error){
+		console.error(error);
+	});
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let loader = new VoxelLoader();
-loader.load("../models/monu2.vox",
-
-function ( voxel ) {
-	console.log("here")
-	scene.add( voxel );
-}
-);
-camera.position.z = 5;
-renderer.render(scene, camera);
 
 function animate(time) {
 	requestAnimationFrame(animate)
 	TWEEN.update(time)
+	renderer.render(scene, camera);
 }
 requestAnimationFrame(animate)
 
@@ -36,21 +51,29 @@ window.mooveCamera = function() {
 			console.log("here")
 			camera.position.x = coords.x
 			camera.position.y = coords.y
-			renderer.render(scene, camera);})
+			renderer.render(scene, camera);
+			console.log(camera.position);
+		})
 		.easing(TWEEN.Easing.Quintic.Out)
 		.start();
+	console.log(camera.position);
 };
 
 window.cameraPlace = function (){
+	console.log("camera place");
 	const coords = { x: camera.position.x, y: camera.position.y };
 	new TWEEN.Tween(coords)
 		.to({ x: 0, y: 0 })
-		.onUpdate(() => {
+		.onUpdate(function() {
 			console.log("here")
 			camera.position.x = coords.x
 			camera.position.y = coords.y
-			renderer.render(scene, camera);})
+			renderer.render(scene, camera);
+			console.log(camera.position);
+
+		})
 		.easing(TWEEN.Easing.Quintic.Out)
 		.start();
+	console.log(camera.position);
 };
 
