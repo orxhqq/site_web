c255cc95bbc084b6eeaa9ac9375a778e6006b617
jean-francois labbe <jean-francois@hey.com>
2022-03-23 17:23:28 +0100

    Try to make the camera move

--- js/projet.js
+++ js/projet.js
@@ -3,9 +3,8 @@ import { VOXLoader, VOXMesh } from '../vendor/jsm/loaders/VOXLoader.js';
 import { OrbitControls } from '../vendor/jsm/controls/OrbitControls.js';
 
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 10000);
+window.camera = camera;
 // camera.position.z = 5;
-camera.position.set( -0.25, -0.10, 0.25, );
-camera.lookAt (0, -1, 2);
 // camera.setViewOffset (0.25, 0.25, 0.25, 0.25, 0.25, 0.25);
 // camera.position.y = z;
 // controls.update();
@@ -37,12 +36,18 @@ loader.load("../models/monu2.vox",
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight);
+window.renderer = renderer;
+window.scene = scene;
 document.body.appendChild(renderer.domElement);
 
 controls = new OrbitControls( camera, renderer.domElement );
 				controls.minDistance = 0;
 				controls.maxDistance = 10;
 				
+camera.position.set( -0.28, 0.0657, 0.442 );
+// camera.lookAt (0, -1, 2);
+// controls.update();
+window.controls = controls;
 
 function animate(time) {
 	requestAnimationFrame(animate)
@@ -54,36 +59,57 @@ function animate(time) {
 requestAnimationFrame(animate)
 
 window.mooveCamera = function() {
-	const coords = { x: camera.position.x, y: camera.position.y };
+	const coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
 	new TWEEN.Tween(coords)
-		.to({ x: 2, y: 1 })
+		.to({ x: 2, y: 1, z: 0 })
 		.onUpdate(() => {
-			console.log("here")
 			camera.position.x = coords.x
 			camera.position.y = coords.y
+			camera.position.z = coords.z
 			renderer.render(scene, camera);
-			console.log(camera.position);
 		})
 		.easing(TWEEN.Easing.Quintic.Out)
 		.start();
+			console.log("move")
+	controls.update();
 	console.log(camera.position);
 };
 
-window.cameraPlace = function (){
+window.place1 = function (){
 	console.log("camera place");
-	const coords = { x: camera.position.x, y: camera.position.y };
+	const coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
 	new TWEEN.Tween(coords)
-		.to({ x: 0, y: 0 })
+		.to({ x: -0.28, y: -0.0657, z: 0.442 })
 		.onUpdate(function() {
-			console.log("here")
 			camera.position.x = coords.x
 			camera.position.y = coords.y
+			camera.position.z = coords.z
 			renderer.render(scene, camera);
-			console.log(camera.position);
 
 		})
 		.easing(TWEEN.Easing.Quintic.Out)
 		.start();
+			console.log("place")
+	controls.update();
+	console.log(camera.position);
+};
+
+window.place2 = function (){
+	console.log("camera place2");
+	const coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
+	new TWEEN.Tween(coords)
+		.to({ x: -0.2835, y: 0.0374, z: -0.04158 })
+		.onUpdate(function() {
+			camera.position.x = coords.x
+			camera.position.y = coords.y
+			camera.position.z = coords.z
+			renderer.render(scene, camera);
+
+		})
+		.easing(TWEEN.Easing.Quintic.Out)
+		.start();
+			console.log("place")
+	controls.update();
 	console.log(camera.position);
 };
 
